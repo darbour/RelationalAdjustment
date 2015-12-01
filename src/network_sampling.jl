f7206e57045268fa1d3114b719fbf88437419ddb
David Arbour <darbour@cs.umass.edu>
2015-12-01 16:39:45 -0500

    added GSN sampling

--- src/network_sampling.jl
+++ src/network_sampling.jl
@@ -19,6 +19,13 @@ function create_covariates(adj_mat, data, aggregate_functions)
     return covariates
 end
 
+function update_covariates!(adj_mat, data, update_cov)
+    agg_functions = [mean]
+    for agg_function in aggregate_functions
+        data["$(update_cov)_$(agg_function)"] = create_aggregate(adj_mat, data[update_cov], agg_function)
+    end
+end
+
 function RPSM(adj_mat, data, treatment)
     agg_functions = [mean]
     covariates = create_covariates(adj_mat, data, agg_functions)
@@ -39,4 +46,24 @@ function RPSM(adj_mat, data, treatment)
     propensity_score_model = GLM(parse(formula), data=covariates, Binomial(), LogitLink())
     
     return propensity_score_model
+end
+
+function GSN(adj_mat, data, treatment, burnin, samples, thinning)
+    psm = RPSM(adj_mat, data, treatment)
+    N = size(adj_mat, 1)
+    cur_sample = rand(Binomial(), N)
+    loop_data = copy(data)
+    update_covariates!(adj_mat, loop_data, treatment)
+    samples = zeros(samples, N)
+    for i in 1:(burnin+(samples*thinning))
+        # choose the next sample at random
+        next_var = sample(1:N, 1)
+        prediction = rand(Binomial(1, predict(loop_data[next_var,:])[1]), 1)
+        loop_data[treatment][next_var] = prediction 
+        update_covariates!(adj_mat, loop_data, treatment)
+        if i > burnin && i % thinning == 0
+            samples[i, :] = loop_data[treatment]
+        end
+    end
+    return samples
 end
