a06a658b81f044a27893d5df90abb9380146945c
Dan Garant <dangarant@gmail.com>
2015-12-01 19:08:08 -0500

    Cleaning up commented code

--- src/ht-estimator.R
+++ src/ht-estimator.R
@@ -13,21 +13,10 @@ ht <- function(exposure, outcome, eprob.fun) {
     exposure.outcome[[cur.exposure]] <- 1/length(exposure) * sum(cur.outcomes / subject.probs)
     
     pairwise.probs <- outer(subjects, subjects, Vectorize(function(i, j) eprob.fun(i, j, cur.exposure)))
-#     term2 <- 0
-#     for(i in 1:length(subjects)) {
-#       term2 <- term2 + (1 - subject.probs[i]) * (cur.outcomes[i] / subject.probs[i]) ** 2
-#       for(j in setdiff(1:length(subjects), i)) {
-#         term2 <- term2 + (eprob.fun(i, j, cur.exposure) - subject.probs[i] * subject.probs[j]) / eprob.fun(i, j, cur.exposure) * cur.outcomes[i] / subject.probs[i] * cur.outcomes[j] / subject.probs[j]
-#       }
-#     }
-    term2 <- (1 - (subject.probs %*% t(subject.probs) / pairwise.probs)) * ((cur.outcomes / subject.probs) %*% t(cur.outcomes / subject.probs))
-    diag(term2) <- 0            
+    corr.comp <- (1 - (subject.probs %*% t(subject.probs) / pairwise.probs)) * ((cur.outcomes / subject.probs) %*% t(cur.outcomes / subject.probs))
+    diag(corr.comp) <- 0            
     exposure.variance[[cur.exposure]] <- 1/(length(exposure)**2) * 
-        (sum((1 - subject.probs) * (cur.outcomes / subject.probs) ** 2) + sum(term2))
-#     cat("M1: ", 1/(length(exposure)**2) * 
-#           (sum((1 - subject.probs) * (cur.outcomes / subject.probs) ** 2) + sum(term2)), "\n")    
-#     cat("M2: ", 1/(length(exposure)**2) * 
-#           (sum((1 - subject.probs) * (cur.outcomes / subject.probs) ** 2) + sum(term2)), "\n")    
+        (sum((1 - subject.probs) * (cur.outcomes / subject.probs) ** 2) + sum(corr.comp))
   }
   return(list("mean"=exposure.outcome, "variance"=exposure.variance))
 }
