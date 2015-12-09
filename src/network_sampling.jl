e0540a61dbef87e724ca6a8fb1464d0fbcdd92bc
Dan Garant <dgarant@cs.umass.edu>
2015-12-09 11:13:41 -0500

    Sampling running

--- src/network_sampling.jl
+++ src/network_sampling.jl
@@ -1,9 +1,10 @@
 using GLM 
+using DataFrames
 
 function create_aggregate(adj_mat, values, aggregate_function)
     vals = zeros(size(adj_mat, 1))
     for i in 1:size(adj_mat, 1)
-        vals[i] = aggregate_function(values[map(Bool, adj_mat[i,:])])
+        vals[i] = aggregate_function(values[map(Bool, adj_mat[i,:]), :])
     end
     return vals
 end
@@ -12,58 +13,67 @@ function create_covariates(adj_mat, data, aggregate_functions)
     # create the data frames
     columns = names(data)
     for column in columns
+        if column == :o
+            continue
+        end
         for agg_function in aggregate_functions
-            data["$(column)_$(agg_function)"] = create_aggregate(adj_mat, data[column], agg_function)
+            agg = create_aggregate(adj_mat, data[column], agg_function)
+            orig_names = names(data)
+            data = hcat(data, agg)
+            new_names = vcat(orig_names, parse("$(column)_$(agg_function)"))
+            names!(data, new_names)
         end   
     end
-    return covariates
+    return data
 end
 
-function update_covariates!(adj_mat, data, update_cov)
+function update_covariates!(adj_mat, data, update_cov )
     agg_functions = [mean]
-    for agg_function in aggregate_functions
-        data["$(update_cov)_$(agg_function)"] = create_aggregate(adj_mat, data[update_cov], agg_function)
+    for agg_function in agg_functions
+        data[parse("$(update_cov)_$(agg_function)")] = create_aggregate(adj_mat, data[parse(update_cov)], agg_function)
     end
 end
 
 function RPSM(adj_mat, data, treatment)
     agg_functions = [mean]
     covariates = create_covariates(adj_mat, data, agg_functions)
-    formula = "$treatment ~ "
-    idx = 0
-    for column in columns
-        if idx > 0
-            formula = string(formula, '+')
-        end
-        idx = idx + 1
-        if column != treatment
-            formula = string(formula, column)
-        end
-        for agg_function in agg_functions
-            formula = string(formula, '+', "$(column)_$(agg_function)")
+    a = []
+    for column in names(covariates)
+        if column != :t && column != :o
+            append!(a, [column])
         end
     end
-    propensity_score_model = GLM(parse(formula), data=covariates, Binomial(), LogitLink())
+    fm = Formula(parse("$treatment"), Expr(:call, :+, a))
+    propensity_score_model = glm(fm, covariates, Binomial(), LogitLink())
+    println(propensity_score_model)
     
-    return propensity_score_model
+    return (propensity_score_model, covariates)
 end
 
-function GSN(adj_mat, data, treatment, burnin, samples, thinning)
-    psm = RPSM(adj_mat, data, treatment)
+function GSN(adj_mat, data, treatment, burnin, nsamples, thinning)
+    (psm, covariates) = RPSM(adj_mat, data, treatment)
     N = size(adj_mat, 1)
     cur_sample = rand(Binomial(), N)
-    loop_data = copy(data)
+    loop_data = copy(covariates)
     update_covariates!(adj_mat, loop_data, treatment)
-    samples = zeros(samples, N)
-    for i in 1:(burnin+(samples*thinning))
+    samples = zeros(nsamples, N)
+    for i in 1:(burnin+(nsamples*thinning))
+        if i % 100 == 0
+            println(i)
+        end
         # choose the next sample at random
         next_var = sample(1:N, 1)
-        prediction = rand(Binomial(1, predict(loop_data[next_var,:])[1]), 1)
-        loop_data[treatment][next_var] = prediction 
+        prediction = rand(Binomial(1, predict(psm, loop_data[next_var,:])[1]), 1)
+        loop_data[:t][next_var] = prediction 
         update_covariates!(adj_mat, loop_data, treatment)
         if i > burnin && i % thinning == 0
-            samples[i, :] = loop_data[treatment]
+            samples[i-burnin, :] = loop_data[:t]
         end
     end
+    println(samples[1:10, 1:10])
     return samples
 end
+
+adj_mat = readcsv("network.csv")
+data = readtable("network_attrs.csv")
+GSN(adj_mat, data, "t", 50, 300, 1)
