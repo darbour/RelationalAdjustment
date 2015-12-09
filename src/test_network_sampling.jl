ce0289466133b3cba1b3f9adec54d501ff772fc0
David Arbour <darbour@cs.umass.edu>
2015-12-09 14:13:40 -0500

    assigned to a variable

--- src/test_network_sampling.jl
+++ src/test_network_sampling.jl
@@ -2,4 +2,4 @@ include("network_sampling.jl")
 
 adj_mat = readcsv("network.csv")
 data = readtable("network_attrs.csv")
-GSN(adj_mat, data, "t", 10000, 5000, 1000)
+samples = GSN(adj_mat, data, "t", 10000, 5000, 1000)
