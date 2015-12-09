724d645d67cd5eb93389e02b0a9ad1ed35a1f88a
David Arbour <darbour@cs.umass.edu>
2015-12-09 16:03:53 -0500

    exposure probs calculated

--- src/generate_data.R
+++ src/generate_data.R
@@ -1,7 +1,7 @@
 library(igraph)
 library(plyr)
 
-nsubjects <- 1000
+nsubjects <- 100
 net <- sample_smallworld(1, nsubjects, 3, 0.1)
 adj.mat <- as.matrix(get.adjacency(net))
 
@@ -11,16 +11,18 @@ c3 <- (adj.mat %*% c2) / rowSums(adj.mat) + rnorm(nsubjects)
 t.prob <- 1 / (1 + exp(-scale(c1 + c2 + c3 + rnorm(nsubjects))))
 treatment <- rbinom(nsubjects, 1, prob=t.prob)
 
-burn.in <- 1000
+burn.in <- 100
 iter <- 0
 while(iter <= burn.in) {
     iter <- iter + 1
-    t.friends <- adj.mat %*% treatment / rowSums(adj.mat)
-    t.prob <- 1 / (1 + exp(-(scale(c1 + c2 + c3 + rnorm(nsubjects)) + scale(t.friends))))
+    t.friends <- adj.mat %*% treatment 
+    t.prob <- 1 / (1 + exp(-(scale(c1 + c2 + c3 + rnorm(nsubjects)) + scale(t.friends)) ) )
     treatment <- rbinom(nsubjects, 1, prob=t.prob)
 }
 
+print(summary(glm(t ~ ., data=data.frame(c1, c2, c3, t=treatment, t.friends))))
+
 o <- c1 + c2 + c3 + treatment + rnorm(nsubjects)
 
-write.csv(data.frame(c1, c2, c3, treatment, o), file="network_attrs.csv", row.names = FALSE)
+write.csv(data.frame(c1, c2, c3, t=treatment, o), file="network_attrs.csv", row.names = FALSE)
 write.table(adj.mat, file="network.csv", row.names = FALSE, col.names=FALSE, sep=",")
