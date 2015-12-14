96632d36418259a46b57f96098922eec45367dd4
Dan Garant <dgarant@cs.umass.edu>
2015-12-14 15:13:02 -0500

    Incorporating pairwise probabilities to get variance estimates

--- src/ht-estimator.R
+++ src/ht-estimator.R
@@ -16,10 +16,25 @@ ht <- function(exposure, outcome, eprob.fun) {
     exposure.outcome[[cur.exposure]] <- 1/n.valid.subjects * sum(cur.outcomes / subject.probs)
     
     pairwise.probs <- outer(subjects, subjects, Vectorize(function(i, j) eprob.fun(i, j, cur.exposure)))
-    corr.comp <- (1 - (subject.probs %*% t(subject.probs) / pairwise.probs)) * ((cur.outcomes / subject.probs) %*% t(cur.outcomes / subject.probs))
+    corr.comp <- ((pairwise.probs - subject.probs %*% t(subject.probs)) / pairwise.probs) * ((cur.outcomes / subject.probs) %*% t(cur.outcomes / subject.probs))
     diag(corr.comp) <- 0            
     exposure.variance[[cur.exposure]] <- 1/(n.valid.subjects**2) * 
-        (sum((1 - subject.probs) * (cur.outcomes / subject.probs) ** 2) + sum(corr.comp))
+        (sum((1 - subject.probs) * (cur.outcomes / subject.probs) ** 2) + 
+           sum(corr.comp * is.finite(corr.comp), na.rm=TRUE))
+    
+    cat("variance", exposure.variance[[cur.exposure]], "\n")
+    # need to correct the variance if there are zero-valued pairwise probabilities
+    zero.vals <- which(pairwise.probs == 0, arr.ind=TRUE)
+    if(nrow(zero.vals) > 0) {
+      bias.correction <- aaply(zero.vals, 1, function(r) {
+        row <- r["row"]
+        col <- r["col"]
+        return(((exposure[row] == cur.exposure) * outcome[row] ** 2) / (2 * subject.probs[[as.character(row)]]) + 
+                 ((exposure[col] == cur.exposure) * outcome[col] ** 2) / (2 * subject.probs[[as.character(col)]]))
+      })
+      cat("Bias correction", sum(bias.correction), "\n")
+      exposure.variance[[cur.exposure]] <- exposure.variance[[cur.exposure]] + sum(bias.correction)
+    }
   }
   return(list("mean"=exposure.outcome, "variance"=exposure.variance))
 }
--- src/test_ht_estimator.R
+++ src/test_ht_estimator.R
@@ -1,72 +1,46 @@
 library(plyr)
+library(stringr)
 library(ggplot2)
+library(Matrix)
 
-source("exposure.R")
 source("ht-estimator.R")
 
 adjmat <- as.matrix(read.csv("network1000.csv", header=FALSE))
-tsamples <- as.matrix(read.csv("samples1000.csv", header=FALSE))
-data <- read.csv("network_attrs1000.csv")
+data <- read.csv("network_attrs1000e.csv")
 
-map.exposures <- function(neighborhood.prop, intrinsic.treatments) {
-    neighborhood.prop.bin <- cut(neighborhood.prop, 
-                breaks=seq(0, 1, length.out=11), include.lowest=TRUE)
-    return(paste(intrinsic.treatments, neighborhood.prop.bin, sep=","))
+
+exposures <- outer(c(0, 1), unique(cut(seq(0, 1, length.out=11), 
+            breaks=seq(0, 1, length.out=11), include.lowest=TRUE)), function(a, b) paste(a, b, sep=","))
+ordered.exposure.cats <- c(exposures[1, ], exposures[2, ])
+obs.exposure <- ordered.exposure.cats[as.numeric(data[, "exposure"])]
+
+# store pairwise exposure probs in a list indexed by exposure category
+pairwise.exposures <- list()
+exp.prob.files <- Sys.glob("network1000_exposure*.csv")
+for(f in exp.prob.files) {
+    # files are tagged by factor level, not name
+    exposure.cat <- as.numeric(str_replace(f, "network.*_exposure([0-9]+).csv", "\\1"))
+    exposure.cat.str <- ordered.exposure.cats[exposure.cat]
+    dat <- read.csv(f)
+    pairwise.exposures[[exposure.cat.str]] <- sparseMatrix(i=dat$row, j=dat$col, x=dat$prob, dims=c(1000, 1000))
 }
 
-# figure out what exposure category we actually observed for each subject
-obs.t <- t(data[, "t"])
-obs.exposure <- calculate.exposure.dists(obs.t, adjmat, prop.exposed)
-obs.exposure <- map.exposures(obs.exposure, obs.t)
-
-# figure out what exposure category each sample corresponds to for each subject
-exposure.dists <- calculate.exposure.dists(tsamples, adjmat, prop.exposed)
-
-# matrix -> vector for efficiency of 'cut'
-discrete.exposure <- map.exposures(c(exposure.dists), c(tsamples)) 
-exposure.cats <- unique(discrete.exposure)
-ordered.exposure.cats <- exposure.cats[order(str_replace_all(exposure.cats, "\\[|\\(|\\]|\\)", ""))]
-
-exposure.mat <- matrix(discrete.exposure, ncol=ncol(exposure.dists))
-
-# compute marginal exposure probabilities
-eprobs <- matrix(0, nrow(adjmat), length(exposure.cats))
-colnames(eprobs) <- exposure.cats
-for(i in 1:nrow(exposure.mat)) {
-    probs <- prop.table(table(exposure.mat[i, ]))
-    eprobs[i, names(probs)] <- probs
-}
-
-# compute joint exposure probabilities, 
-# store in a list indexed by exposure category
-
-# this is too slow:
-#subjects <- 1:nrow(exposure.mat)
-#pairwise.probs <- llply(exposure.cats, function(exposure.cat) {
-#    outer(subjects, subjects, function(i, j) {
-#        sum(exposure.mat[i, ] == exposure.cat && exposure.mat[j, ] == exposure.cat) / ncol(exposure.mat)
-#    })
-#})
-
-
-# TODO: need to compute pairwise probs 
-# (tensor of size subjects x subjects x num exposures)
 eprob.fun <- function(i, j, e) {
   if(i == j) {
-    return(eprobs[i, e])
+    return(pairwise.exposures[[e]][i, j])
   } else {
-    return(NA)
+    return(pairwise.exposures[[e]][i, j])
   }
 }
 estimates <- ht(obs.exposure, data[, "o"], eprob.fun)
 print(estimates)
-d <- data.frame(exposure=factor(names(estimates$mean), levels=ordered.exposure.cats), outcome=unlist(estimates$mean))
+d <- data.frame(exposure=factor(names(estimates$mean), levels=ordered.exposure.cats), 
+                outcome=unlist(estimates$mean), variance=unlist(estimates$variance))
 
-# TODO: need variance estimates for confidence intervals
-g <- ggplot(d, aes(x=exposure, y=outcome)) + 
-    geom_point() + theme_bw(base_size=16) + labs(x="Exposure", y="Mean Outcome")
+g <- ggplot(d, aes(x=exposure, y=outcome)) + geom_errorbar(aes(ymin=outcome-sqrt(variance), ymax=outcome+sqrt(variance))) +
+    geom_point() + theme_bw(base_size=16) + labs(x="Exposure", y="Mean Outcome") + theme(axis.text.x=element_text(angle=45))
 print(g)
-png("exposure-trend.png", width=800, height=400)
+png("exposure-trend.png", width=1200, height=400)
 print(g)
 dev.off()
 
