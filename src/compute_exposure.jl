1a5e9f875364b7bc5403795f81a845b5aa1cbe6f
Dan Garant <dgarant@cs.umass.edu>
2015-12-28 13:33:22 -0500

    Full pipeline in Julia. Still needs some work

--- src/compute_exposure.jl
+++ src/compute_exposure.jl
@@ -66,24 +66,3 @@ function get_exposures(obs_treatment, treatment_samples, adj_mat, exposurefun=pr
     return(obs_exposures, exposure_probs)
 end
 
-samples = readcsv("samples1000.csv")
-adj_mat = readcsv("network1000.csv")
-data = readtable("network_attrs1000.csv")
-(obs_exposures, exposure_probs) = get_exposures(data[:, :t], samples, adj_mat)
-
-for exposure in 1:20
-    probs = exposure_probs[exposure]
-    # convert to a tabular format for writing
-    (rowIdx, colIdx) = findn(probs)
-    output = DataFrame([Int, Int, Float64], [:row, :col, :prob], size(rowIdx, 1))
-    for i in 1:size(rowIdx, 1)
-        output[i, :row] = rowIdx[i]
-        output[i, :col] = colIdx[i]
-        output[i, :prob] = probs[rowIdx[i], colIdx[i]]
-    end
-    writetable("network1000_exposure$(exposure).csv", output)
-end
-data[:exposure] = obs_exposures
-
-writetable("network_attrs1000e.csv", data)
-
--- src/simulate.jl
+++ src/simulate.jl
@@ -1,34 +1,35 @@
-using RCall
+#n_subjects = 1000
+#burn_in = 10000
+#samples = 5000
+#thinning = 1000
 
-rcopy("source('generate_data.R')")
+n_subjects = 1000
+burn_in = 10
+samples = 500
+thinning = 10
 
-## Parameters
-n_subjects = 100
-burn_in = 10000
-samples = 5000
-thinning = 1000
+include("generate_data.jl")
+include("network_sampling.jl")
+include("ht_estimator.jl")
+include("compute_exposure.jl")
 
 # generate some data
-data, adj_mat = rcopy("generate.data($(n_subjects))")
+println("Generating data for $n_subjects subjects ...")
+adj_mat, dat = generate_data(n_subjects)
 
 # now simulate from the joint exposure distribution
-include("network_sampling.jl")
-samples = GSN(adj_mat, data, "t", burn_in, samples, thinning)
+println("Gathering $samples samples ...")
+samples = GSN(adj_mat, dat, "t", burn_in, samples, thinning)
+
 
 # calculate the exposure probs
-(obs_exposures, exposure_probs) = get_exposures(data[:, :t], samples, adj_mat)
-for exposure in 1:20
-    probs = exposure_probs[exposure]
-    # convert to a tabular format for writing
-    (rowIdx, colIdx) = findn(probs)
-    output = DataFrame([Int, Int, Float64], [:row, :col, :prob], size(rowIdx, 1))
-    for i in 1:size(rowIdx, 1)
-        output[i, :row] = rowIdx[i]
-        output[i, :col] = colIdx[i]
-        output[i, :prob] = probs[rowIdx[i], colIdx[i]]
-    end
-    writetable("network1000_exposure$(exposure).csv", output)
-end
-data[:exposure] = obs_exposures
+(obs_exposures, exposure_probs) = get_exposures(dat[:, :t], samples, adj_mat)
 
-writetable("network_attrs1000e.csv", data)
+println("Estimating means and variances ...")
+means, variances = ht(obs_exposures, dat[:o], exposure_probs)
+
+println("Means:")
+println(means)
+
+println("Variances:")
+println(variances)
