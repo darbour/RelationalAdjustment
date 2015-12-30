4d2504191b3725d82fc6733fe0a348485715763f
Dan Garant <dgarant@cs.umass.edu>
2015-12-30 15:04:44 -0500

    Fixing some bugs in the estimator

--- src/ht_estimator.jl
+++ src/ht_estimator.jl
@@ -15,6 +15,7 @@ function ht(exposure, outcome, exposure_probs)
 
     exposure_outcomes = Dict()
     exposure_variances = Dict()
+    variance_biased = Dict()
     for(cur_exposure in unique_exposures)
         eprobs = exposure_probs[cur_exposure]
 
@@ -38,23 +39,32 @@ function ht(exposure, outcome, exposure_probs)
         end
 
         corr_comp = ((pairwise_probs - subject_eprobs * subject_eprobs') ./ pairwise_probs) .* ((subject_outcomes ./ subject_eprobs) * (subject_outcomes ./ subject_eprobs)')
-        corr_comp[diagind(corr_comp)] = 0
+        corr_comp[diagind(corr_comp)] = 0.0
 
-        exposure_variances[cur_exposure] = 1/(n_valid_subjects^2) .* (sum((1 - subject_eprobs) .* (subject_outcomes ./ subject_eprobs) .^ 2) + sum(corr_comp .* isfinite(corr_comp)))
+        term1 = map(Real, (1 - subject_eprobs) .* (subject_outcomes ./ subject_eprobs) .^ 2)
+        exposure_variances[cur_exposure] = 1/(n_valid_subjects^2) .* (sum(term1[map(Bool, isfinite(term1))]) + sum(corr_comp[map(Bool, isfinite(corr_comp))]))
 
         # need to correct the variance if there are zero-valued off-diagonal pairwise probabilities
         (rows, cols, probs) = findnz((pairwise_probs + eye(n_cur_subjects)) .== 0)
         bias_correction = 0
         for(i in 1:size(rows, 1))
-            row = rows[i]
-            col = cols[i]
-            bias_correction += (exposure[row] == cur_exposure)*outcome[row]^2/(2 * eprobs[row,row]) + (exposure[col] == cur_exposure) * outcome[col]^2 / (2 * eprobs[col, col])
+            row = subjects[rows[i]]
+            col = subjects[cols[i]]
+            val1 = (exposure[row] == cur_exposure)*outcome[row]^2/(2 * eprobs[row, row]) 
+            val2 = (exposure[col] == cur_exposure) * outcome[col]^2 / (2 * eprobs[col, col])
+            bias_correction += (isfinite(val1) ? val1 : 0) + (isfinite(val2) ? val2 : 0)
         end
+        if bias_correction > 0
+            variance_biased[cur_exposure] = true
+        else
+            variance_biased[cur_exposure] = false
+        end
+        println("Exposure condition $cur_exposure witnessed in $n_valid_subjects subjects, mean exposure prob: $(round(mean(eprobs[diagind(eprobs)]), 2))")
         exposure_variances[cur_exposure] = exposure_variances[cur_exposure] + bias_correction
 
     end
     
-    return(exposure_outcomes, exposure_variances)
+    return(exposure_outcomes, exposure_variances, variance_biased)
 end
 
 
--- src/simulate.jl
+++ src/simulate.jl
@@ -1,25 +1,30 @@
-#n_subjects = 1000
-#burn_in = 10000
-#samples = 5000
-#thinning = 1000
 
 n_subjects = 1000
-burn_in = 10
-samples = 500
-thinning = 10
+burn_in = 10000
+n_samples = 50000
+thinning = 1000
 
-include("generate_data.jl")
 include("network_sampling.jl")
 include("ht_estimator.jl")
 include("compute_exposure.jl")
+
+#println("Running Add Health simulation ...")
+#include("generate_addhealth_data.jl")
+#adj_mat, dat = generate_addhealth_data()
+#samples = GSN(adj_mat, dat, "t", burn_in, n_samples, thinning)
+#(obs_exposures, exposure_probs) = get_exposures(dat[:, :t], samples, adj_mat, get_addhealth_exposure, 4)
+#means, variances = ht(obs_exposures, dat[:o], exposure_probs)
+
+println("Running logistic simulation ...")
+include("generate_data.jl")
 
 # generate some data
 println("Generating data for $n_subjects subjects ...")
 adj_mat, dat = generate_data(n_subjects)
 
 # now simulate from the joint exposure distribution
-println("Gathering $samples samples ...")
-samples = GSN(adj_mat, dat, "t", burn_in, samples, thinning)
+println("Gathering $n_samples samples ...")
+samples = GSN(adj_mat, dat, "t", burn_in, n_samples, thinning)
 
 
 # calculate the exposure probs
