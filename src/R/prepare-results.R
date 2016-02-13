a7eb8a446fedfb3c412102fe8bdd46da7f61b1d1
Dan Garant <dgarant@cs.umass.edu>
2016-02-12 22:31:42 -0500

    results plots

--- src/R/prepare-results.R
+++ src/R/prepare-results.R
@@ -26,8 +26,77 @@ results.with.actuals <- merge(melt.results, subset(subset(melt.results, method==
 results.with.config <- merge(results.with.actuals, configuration, by.x="config", by.y="X")
 
 results.with.config$global.effect.err <- results.with.config$global.effect.est - results.with.config$global.effect.act
-single.inst.per.trial <- subset(results.with.config, setting == 0 & graph.cluster.randomization == TRUE & 
-                                  ((of.beta == 1 & ot.beta == 1) | (of.beta == 2 & ot.beta == 2 & exposure.type %in% c("exponential", "sigmoid"))))
+
+specific.results <- subset(results.with.config, of.beta == 10 & ot.beta == 10 &
+                           ((graph.cluster.randomization == FALSE & treatment.autocorr.coeff == 2 & confounding.coeff == 1 &
+                              method %in% c("Obs-GBM-Sufficient", "Actual")) | (method  == "Exp-LM-IND" & graph.cluster.randomization == TRUE)) &
+                             exposure.type == "sigmoid" & graph.type == "small-world" & p == 0.1)
+g <- ggplot(specific.results, aes(x=setting, y=value.est, color=method)) + 
+  geom_smooth(size=1) + theme_bw(base_size=20) + labs(x=expression(theta), y="Outcome") + 
+  scale_color_manual(breaks=c("Actual", "Obs-GBM-Sufficient", "Exp-LM-IND"), 
+                       labels=c("Actual", "Obs. GBM", "Exp. LM"), values=c(1, 3, 2), name="Method") +
+  theme(legend.position="top") + guides(color=guide_legend(ncol=2))
+print(g)
+png("sigmoid-example.png", width=400, height=300)
+print(g)
+dev.off()
+
+
+observational.configs <- subset(results.with.config, 
+                                setting == 0 & graph.cluster.randomization == FALSE & 
+                                  ((of.beta == 1 & ot.beta == 1) | (of.beta == 2 & ot.beta == 2 & exposure.type %in% c("sigmoid"))) & 
+                                  method %in% c("Obs-GBM-Sufficient", "Obs-LM-Sufficient", "Exp-GBM"))
+observational.configs$method <- revalue(observational.configs$method, 
+                                        c("Obs-GBM-Sufficient"="Obs. GBM", "Obs-LM-Sufficient"="Obs. LM", 
+                                          "Exp-GBM"="Obs. GBM\n(unadj)"))
+experimental.configs <- subset(results.with.config, 
+                               setting == 0 & graph.cluster.randomization == TRUE &
+                                 ((of.beta == 1 & ot.beta == 1) | (of.beta == 2 & ot.beta == 2 & exposure.type %in% c("sigmoid"))) & 
+                                 method %in% c("Exp-GBM", "Exp-HT", "Exp-LM-IND"))
+experimental.configs$method <- revalue(experimental.configs$method, 
+                                        c("Exp-GBM"="Exp. GBM", "Exp-HT"="Exp. HT", 
+                                          "Exp-LM-IND"="Exp. LM"))
+compare.exp.obs <- rbind(observational.configs, experimental.configs)
+g <- ggplot(subset(compare.exp.obs, exposure.type == "linear" & method != "Obs. LM"), 
+       aes(x=method, y=global.effect.err, fill=method)) + geom_boxplot(notch=TRUE) + theme_bw() + guides(fill="none") + 
+      labs(x="Method", y="Global ATE Error") + geom_vline(xintercept=2.5, size=1, linetype="longdash", color="gray47") +
+      scale_fill_manual(values=c("Exp. HT"="slateblue", "Exp. LM"="gray47", "Obs. GBM"="firebrick", "Obs. LM"="deepskyblue3", "Obs. GBM\n(unadj)"="indianred2"))
+print(g)
+png("exp-obs-compare.png", width=400, height=300)
+print(g)
+dev.off()
+
+experimental.configs2 <- merge(subset(experimental.configs, select=-confounding.coeff), data.frame(confounding.coeff=c(1, 3)))
+compare.exp.obs <- rbind(observational.configs, experimental.configs2)
+plot.compare.dat <- subset(compare.exp.obs, exposure.type != "exponential" & method != "Obs. LM" & 
+                             (treatment.autocorr.coeff == 2 | 
+                                (treatment.autocorr.coeff == 0 & graph.cluster.randomization == TRUE)))
+plot.compare.dat$exposure.type <- revalue(plot.compare.dat$exposure.type, c("linear"="Linear", "rbf-friends"="RBF", "sigmoid"="Sigmoid"))
+plot.compare.dat$confounding.coeff <- paste0("Confounding ", plot.compare.dat$confounding.coeff)
+g <- ggplot(plot.compare.dat, aes(x=method, y=global.effect.err, fill=method)) + 
+    geom_violin(scale="width") + facet_grid(confounding.coeff~exposure.type, scales="free") + 
+    theme_bw(base_size=16) + guides(fill="none") + labs(x="Method", y="Global ATE Error") + geom_vline(xintercept=2.5, linetype="longdash", color="gray47")
+print(g)
+png("cross-exposure-compare.png", width=1000, height=300)
+print(g)
+dev.off()
+
+single.inst.per.trial <- subset(results.with.config, setting == 0 & method != "Actual" & 
+                                  ((graph.cluster.randomization == TRUE & method %in% c("Exp-LM-IND")) | 
+                                     (graph.cluster.randomization == FALSE & method %in% c("Obs-GBM-Sufficient") & 
+                                        treatment.autocorr.coeff == 2 & confounding.coeff == 1)
+                                  ))
+single.inst.per.trial$indiv.effect.err <- sqrt((single.inst.per.trial$indiv.effect.est - single.inst.per.trial$indiv.effect.act) ** 2)
+indiv.effect.perf <- ddply(subset(single.inst.per.trial, ((of.beta == 5 & ot.beta == 5) | (of.beta == 2 & ot.beta == 2 & exposure.type == "sigmoid")) & exposure.type != "exponential"), 
+      .(method, exposure.type), summarize, mean.err = mean(indiv.effect.err), sd.err = sd(indiv.effect.err))
+indiv.effect.perf$cell <- paste0(round(indiv.effect.perf$mean.err, 4), " (", round(indiv.effect.perf$sd.err, 4), ")")
+indiv.effect.perf <- subset(indiv.effect.perf, select=-c(mean.err, sd.err))
+results.table <- reshape(indiv.effect.perf, timevar ="method", direction="wide", idvar="exposure.type")
+colnames(results.table) <- c("", "Exp. LM", "Obs. GLM")
+library(xtable)
+print(xtable(results.table), row.names=FALSE)
+
+
 experimental.results <- ddply(single.inst.per.trial, .(method, exposure.type, graph.type), summarize, mean.err = mean(global.effect.err), sd.err=sd(global.effect.err))
 experimental.results$exposure.type <- revalue(experimental.results$exposure.type, c("exponential"="Exponential", "linear"="Linear", "rbf-friends"="RBF", "sigmoid"="Sigmoid"))
 experimental.results$graph.type <- revalue(experimental.results$graph.type, c("barabasi-albert"="Pref.\nAttach.", "small-world"="Small\nWorld"))
@@ -47,17 +116,20 @@ results.summ <- ddply(subset(results.prune.exponential, !is.na(value.est)), .(co
                       summarize, rmse = sqrt(sum((value.est - value.act)^2) / length(value.act)))
 # bring the configuration back
 results.summ <- merge(results.summ, configuration, by.x="config", by.y="X")
-
-table.by.method <- ddply(subset(results.summ, graph.cluster.randomization == FALSE & 
-                                  effect.type == "Marginal Peer" & method != "Actual" & 
-                                  treatment.autocorr.coeff == 2 & method %in% c("Exp-LM-IND", "Obs-GBM-Sufficient", "Obs-LM-Simple", "Obs-LM-Sufficient")), 
-                            .(exposure.type.x, method, of.beta, ot.beta), summarize, meanrmse = mean(rmse), sdrmse=sd(rmse))
+  
+table.by.method <- ddply(subset(results.summ, effect.type == "Marginal Peer" & method != "Actual" & 
+                                ((graph.cluster.randomization == TRUE & method %in% c("Exp-LM-IND")) | 
+                                   (graph.cluster.randomization == FALSE & method %in% c("Obs-GBM-Sufficient") & 
+                                      treatment.autocorr.coeff == 2 & confounding.coeff == 1)
+                                )), .(exposure.type.x, method, of.beta, ot.beta), 
+                            summarize, meanrmse = mean(rmse), sdrmse=sd(rmse))
 table.by.method <- subset(subset(table.by.method, of.beta >= 2 & ot.beta >= 2), select=-c(of.beta, ot.beta))
-table.by.method$contents <- with(table.by.method, paste0(round(meanrmse, ifelse(meanrmse > 100, 0, 4)), " (", round(sdrmse, ifelse(meanrmse > 100, 0, 3))))
+table.by.method$contents <- with(table.by.method, paste0(round(meanrmse, ifelse(meanrmse > 100, 0, 4)), " (", round(sdrmse, ifelse(meanrmse > 100, 0, 3)), ")"))
 table.by.method$exposure.type.x <- revalue(table.by.method$exposure.type.x, c("exponential"="Exponential", "linear"="Linear", "rbf-friends"="RBF", "sigmoid"="Sigmoid"))
-table.by.method$method <- revalue(table.by.method$method, c("Exp-LM-IND"="Unadjusted LM", "Obs-GBM-Sufficient"="GBM", "Obs-LM-Simple"="Insufficient LM", "Obs-LM-Sufficient"="Sufficient LM"))
-results.table <- reshape(table.by.method, timevar ="exposure.type.x", direction="wide", idvar="method")
-colnames(results.table) <- str_replace(colnames(results.table), "meanrmse.", "")
+table.by.method$method <- revalue(table.by.method$method, c("Exp-LM-IND"="Exp. LM", "Obs-GBM-Sufficient"="Obs. GBM", "Exp-GBM"="Obs. GBM (unadj)"))
+table.by.method <- subset(table.by.method, select=-c(meanrmse, sdrmse))
+results.table <- reshape(table.by.method, timevar ="method", direction="wide", idvar="exposure.type.x")
+colnames(results.table) <- str_replace(colnames(results.table), "contents.", "")
 library(xtable)
 print(xtable(results.table), row.names=FALSE)
 
